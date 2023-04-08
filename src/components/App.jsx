d72133d7e706d8ef05738d5ab6a3e4fa8c25d63b
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-04-09 02:43:45 +0300

    first finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,8 @@ import Feedback from 'components/Feedback';
 
 export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Feedback/>
+    <div>
+      <Feedback />
     </div>
   );
-};
-
+}
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,62 +1,54 @@
 import React from 'react';
-import css from "components/Feedback.module.css"
-
+import css from 'components/Feedback/Feedback.module.css';
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackMarkup from 'components/Feedback/FeedbackMarkup';
 
 class Feedback extends React.Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-      }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    visible: false,
+  };
 
-    handleGood = () => {
-        this.setState(prevState => {return{
-            good: prevState.good + 1
-        }})
-        
-    }
-    handleNeutral = () => {
-        this.setState(prevState => {return{
-            neutral: prevState.neutral + 1
-        }})
-    }
-    handleBad = () => {
-        this.setState(prevState => {return{
-            bad: prevState.bad + 1
-        }})
-    }
+  handleClick = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+      total: prevState.total + 1,
+      visible: true,
+    }));
+  };
+  getPositivePercentage = () => {
+    const { good, total } = this.state;
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
 
+  getTotal = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
 
-    render() {
-        return(
-            <div>
-                <p className={css.title}>Pleace leave feedback</p>
-                <ul className={css.list}>
-                  <li>
-                    <button className={css.button} type="button" 
-                    onClick={this.handleGood}
-                    >
-                      Good
-                    </button>
-                  </li>
-                  <li>
-                    <button className={css.button} type="button" 
-                    onClick={this.handleNeutral}
-                    >
-                      Neutral
-                    </button>
-                  </li>
-                  <li>
-                    <button className={css.button} type="button" 
-                    onClick={this.handleBad}
-                    >
-                      Bad
-                    </button>
-                  </li>
-                </ul>
-              </div>
-              )
-    }
+  render() {
+    const { good, neutral, bad, total } = this.state;
+    return (
+      <div className={css.container}>
+        <FeedbackMarkup
+          clickGood={() => this.handleClick('good')}
+          clickNeutral={() => this.handleClick('neutral')}
+          clickBad={() => this.handleClick('bad')}
+        />
+
+        <Statistics
+          upGood={good}
+          upNeutral={neutral}
+          upBad={bad}
+          upTotal={total}
+          upInterest={this.getPositivePercentage()}
+        />
+      </div>
+    );
+  }
 }
 
-export default Feedback
+export default Feedback;
